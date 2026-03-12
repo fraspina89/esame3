ce1de111fcced425ec275678ae88ead62b177302
Francesco <francescospinazzola084@gmail.com>
2026-03-12 10:39:07 +0100

    Aggiornamento contatti.php

--- contatti.php
+++ contatti.php
@@ -140,13 +140,13 @@ echo head('contatti', $arrCss);
             <?php endif; ?>
 
             <!-- BLOCCO MAPPA -->
-            <div class="mappa">
+            <!-- <div class="mappa">
                 <h2>INDICAZIONE MAPPA</h2>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2821.463760144219!2d7.645665275037975!3d44.995203464610405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47881244e0d5897b%3A0x1b7ab3f8d62eb5eb!2sVia%20Giuseppe%20Giusti%2C%203%2C%2010042%20Nichelino%20TO!5e0!3m2!1sit!2sit!4v1716892363978!5m2!1sit!2sit"
                     width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+            </div> -->
         </div> <!-- chiude .contenitore-form-mappa -->
 
         <!-- FOOTER visibile solo se il form NON è stato inviato -->
@@ -156,13 +156,13 @@ echo head('contatti', $arrCss);
                     <ul>
                         <li>
                             <p>vienici a trovare in:</p>
-                            <address>Largo Giusti,3<br>10100 Nichelino (TO)<br>Italia</address>
+                            <address>.....<br>....<br>.....</address>
                         </li>
                         <li>
                             <p>email - telefono:</p>
                             <ul>
-                                <li><a href="mailto:francescospinazzola084@gmail.com">francescospinazzola084@gmail.com</a></li>
-                                <li><a href="tel:3402330981">3402330981</a></li>
+                                <li><a href="mailto:...">...</a></li>
+                                <li><a href="tel:...">....</a></li>
                             </ul>
                         </li>
                         <li>
